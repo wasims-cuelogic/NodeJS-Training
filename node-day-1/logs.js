88752a762347f4449c9b3bf410ce6f1386685c02
Wasim <wasim.sayyed@cuelogic.com>
2017-01-06 18:29:55 +0530

    Changed logs file

--- node-day-1/logs.js
+++ node-day-1/logs.js
@@ -1,3 +1,5 @@
+var emitter = require('events').EventEmitter;
+
 var log = {
     info: function (info) {
         console.log('Info: ' + info);
@@ -18,3 +20,31 @@ var testVar = "This is a string";
 console.log(`String is ${testVar}`);
 console.log("1=>", process.argv.length);
 console.log(process.argv);
+
+
+function LoopProcessor(num) {
+    var e = new emitter();
+
+    setTimeout(function () {
+
+        for (var i = 1; i <= num; i++) {
+            e.emit('BeforeProcess', i);
+
+            console.log('Processing number:' + i);
+
+            e.emit('AfterProcess', i);
+        }
+    }, 2000)
+
+    return e;
+}
+
+var lp = LoopProcessor(3);
+
+lp.on('BeforeProcess', function (data) {
+    console.log('About to start the process for ' + data);
+});
+
+lp.on('AfterProcess', function (data) {
+    console.log('Completed processing ' + data);
+});
