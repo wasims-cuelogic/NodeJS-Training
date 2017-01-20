b52fa92a7fec2e06cd06291ef4725224997070b5
Wasim <wasim.sayyed@cuelogic.com>
2017-01-20 13:08:17 +0530

    Changed folder name from schemas to schemas-validate

---
+++ assignment/api/users/schemas-validate/authenticateUser.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Joi = require('joi');
+
+var authenticateUserSchema = Joi.alternatives().try(Joi.object({
+    username: Joi.string().alphanum().min(2).max(30).required(),
+    password: Joi.string().required()
+}), Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+}));
+
+module.exports = authenticateUserSchema;
---
+++ assignment/api/users/schemas-validate/updateUser.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+var payloadSchema = Joi.object({
+    fname: Joi.string(),
+    lname: Joi.string(),
+    username: Joi.string().alphanum().min(2).max(30),
+    email: Joi.string().email(),
+    admin: Joi.boolean()
+});
+
+var paramsSchema = Joi.object({
+    id: Joi.objectId().required()
+});
+
+module.exports = {
+    payloadSchema: payloadSchema,
+    paramsSchema: paramsSchema
+};
